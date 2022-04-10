6426812c90e59f23be834c1083355da8e753da1d
Kot <kot@yukata.tech>
2022-04-10 12:33:02 -0700

    Support TLS, simplify usage

--- examples/bebop.rs
+++ examples/bebop.rs
@@ -1,11 +1,9 @@
-use hyper::client::HttpConnector;
-use hyper_tls::HttpsConnector;
 use jikan::{JikanClient, JikanResult};
 
 #[tokio::main]
 async fn main() -> JikanResult<()> {
-    let client = JikanClient::<HttpsConnector<HttpConnector>>::default();
-    dbg!(client.get_anime_by_id(1).await?);
+    let client = JikanClient::default().with_api_url("");
+    println!("{:#?}", client.get_anime_by_id(1).await?);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 #![forbid(unsafe_code)]
 
 pub use hyper;
+#[cfg(feature = "tls")]
 pub use hyper_tls;
 
 pub mod models;
 
 use hyper::{body::Buf, client::HttpConnector, Body, Response};
-use hyper_tls::HttpsConnector;
 use models::{anime::Anime, JikanResponse};
 use thiserror::Error;
+
+#[cfg(feature = "tls")]
+use hyper_tls::HttpsConnector;
 
 pub const DEFAULT_API_URL: &str = "https://api.jikan.moe/v4";
 
@@ -26,19 +29,56 @@ pub enum JikanError {
     Unknown,
 }
 
-pub struct JikanClient<C> {
+#[cfg(feature = "tls")]
+pub type DefaultConnector = HttpsConnector<HttpConnector>;
+
+#[cfg(not(feature = "tls"))]
+pub type DefaultConnector = HttpConnector;
+
+#[derive(Clone)]
+pub struct JikanClient<C = DefaultConnector> {
     pub api_url: String,
     pub http_client: hyper::Client<C>,
 }
 
-impl<C: hyper::client::connect::Connect + Clone + Send + Sync + 'static> JikanClient<C> {
-    pub fn new(http_client: hyper::Client<C>, api_url: String) -> Self {
+impl<C> JikanClient<C> {
+    pub fn new<S: Into<String>>(api_url: S, http_client: hyper::Client<C>) -> Self {
         JikanClient {
+            api_url: api_url.into(),
             http_client,
-            api_url,
         }
     }
 
+    pub fn with_http_client(mut self, http_client: hyper::Client<C>) -> Self {
+        self.http_client = http_client;
+        self
+    }
+
+    pub fn with_api_url<S: Into<String>>(mut self, api_url: S) -> Self {
+        self.api_url = api_url.into();
+        self
+    }
+}
+
+#[cfg(feature = "tls")]
+impl Default for JikanClient<HttpsConnector<HttpConnector>> {
+    fn default() -> Self {
+        let https = HttpsConnector::new();
+        JikanClient::new(
+            String::from(DEFAULT_API_URL),
+            hyper::Client::builder().build::<_, hyper::Body>(https),
+        )
+    }
+}
+
+#[cfg(not(feature = "tls"))]
+impl Default for JikanClient<HttpConnector> {
+    fn default() -> Self {
+        JikanClient::new(String::from(DEFAULT_API_URL), hyper::Client::new())
+    }
+}
+
+impl<C: hyper::client::connect::Connect + Clone + Send + Sync + 'static> JikanClient<C> {
     async fn parse_json_response<T: for<'de> serde::Deserialize<'de>>(
         mut res: Response<Body>,
     ) -> JikanResult<T> {
@@ -61,29 +101,13 @@ impl<C: hyper::client::connect::Connect + Clone + Send + Sync + 'static> JikanCl
     }
 }
 
-impl Default for JikanClient<HttpConnector> {
-    fn default() -> Self {
-        JikanClient::new(hyper::Client::new(), String::from(DEFAULT_API_URL))
-    }
-}
-
-impl Default for JikanClient<HttpsConnector<HttpConnector>> {
-    fn default() -> Self {
-        let https = HttpsConnector::new();
-        JikanClient::new(
-            hyper::Client::builder().build::<_, hyper::Body>(https),
-            String::from(DEFAULT_API_URL),
-        )
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::{hyper::client::HttpConnector, hyper_tls::HttpsConnector, JikanClient};
+    use crate::JikanClient;
 
     #[tokio::test]
     async fn cowboy_bebop() {
-        let client = JikanClient::<HttpsConnector<HttpConnector>>::default();
+        let client = JikanClient::default();
         let anime = client.get_anime_by_id(1).await.unwrap();
         assert_eq!(anime.title, "Cowboy Bebop");
         assert_eq!(anime.year, 1998);
