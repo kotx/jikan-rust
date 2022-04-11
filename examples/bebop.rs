c458c7a67474a9360a1991d8bcce71fa05942bd2
Kot <kot@yukata.tech>
2022-04-10 18:16:29 -0700

    Fix bebop example url

--- examples/bebop.rs
+++ examples/bebop.rs
@@ -2,7 +2,8 @@ use jikan::{JikanClient, JikanResult};
 
 #[tokio::main]
 async fn main() -> JikanResult<()> {
-    let client = JikanClient::default().with_api_url("");
+    let client = JikanClient::default()
+        .with_api_url(std::env::var("JIKAN_API_URL").unwrap_or(jikan::DEFAULT_API_URL.into()));
     println!("{:#?}", client.get_anime_by_id(1).await?);
 
     Ok(())
